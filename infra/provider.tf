0607809456baa79c5abc1709048256cc4bf934fb
Jessica Fung <jessica-fcc@hotmail.com>
2022-12-10 22:09:08 +0100

    updated provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -2,8 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.40.0"
+      version = "4.33.0"
     }
   }
-
+  backend "s3" {
+    bucket = "pgr301-2022-terraform-state"
+    key    = "1044/shopifly.state"
+    region = "eu-north-1"
+  }
 }
