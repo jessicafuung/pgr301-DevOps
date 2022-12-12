93ec29028b3350bd7f6a7fb72b3d4303447d1b69
Jessica Fung <jessica-fcc@hotmail.com>
2022-12-12 01:33:51 +0100

    try sns

--- infra/sns_topic_for_alarm.tf
+++ infra/sns_topic_for_alarm.tf
@@ -1,6 +1,6 @@
 resource "aws_sns_topic" "alarms" {
   name = "alarm-topic-${var.candidate_id}"
-  namespace                 = "1044"
+  namespace                 = "${var.candidate_id}"
   metric_name               = "carts_count.value"
 
   comparison_operator       = "GreaterThanThreshold"
@@ -19,8 +19,4 @@ resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
   topic_arn = aws_sns_topic.alarms.arn
   protocol  = "email"
   endpoint  = "chfu003@student.kristiania.no"
-}
-
-resource "aws_sns_topic" "user_updates" {
-  name = var.candidate_id
 }
