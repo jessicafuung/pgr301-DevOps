bb59f6b415e4411132297f5530089fe23c107986
Jessica Fung <jessica-fcc@hotmail.com>
2022-12-12 05:33:39 +0100

    timed

--- src/main/java/no/shoppifly/ShoppingCartController.java
+++ src/main/java/no/shoppifly/ShoppingCartController.java
@@ -1,7 +1,6 @@
 package no.shoppifly;
 
 import io.micrometer.core.annotation.Timed;
-import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
