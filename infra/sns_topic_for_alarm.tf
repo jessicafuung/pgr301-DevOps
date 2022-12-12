82c775807552183db79146b62efda3120b1ef5cf
Jessica Fung <jessica-fcc@hotmail.com>
2022-12-12 02:20:28 +0100

    try this

--- infra/sns_topic_for_alarm.tf
+++ infra/sns_topic_for_alarm.tf
@@ -7,3 +7,20 @@ resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
   protocol  = "email"
   endpoint  = var.candidate_email
 }
+
+resource "aws_cloudwatch_metric_alarm" "over5" {
+  alarm_name                = "carts-over-5"
+  namespace                 = var.candidate_id
+  metric_name               = "carts_count.value"
+
+  comparison_operator       = "GreaterThanThreshold"
+  threshold                 = "5"
+  evaluation_periods        = "3"
+  period                    = "300"
+
+  statistic                 = "Maximum"
+
+  alarm_description         = "Alarm for carts count exceeding 5 for 3 consecutive periods of 5 minutes"
+  insufficient_data_actions = []
+  alarm_actions       = [aws_sns_topic.alarms.arn]
+}
